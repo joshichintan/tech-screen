1ad31dd99d4ab6e330f78497013df967adda0ec8
Scott Shumway <shumwaysr@gmail.com>
2023-09-18 20:45:19 -0400

    feat: Improve output when not matching

--- order-trade-matching/domain/models.py
+++ order-trade-matching/domain/models.py
@@ -3,27 +3,38 @@ from datetime import datetime
 from decimal import Decimal
 from typing import Literal
 
+TDirection = Literal["buy", "sell"]
 
-@dataclass
+
+@dataclass(order=True)
 class Order:
     symbol: str
     quantity: Decimal
-    direction: Literal["buy", "sell"]
+    direction: TDirection
     submitted_date: datetime
 
+    def __repr__(self) -> str:
+        return f"Order({self.symbol}, {self.direction}, {self.submitted_date}, {self.quantity})"
 
-@dataclass
+
+@dataclass(order=True)
 class Trade:
     symbol: str
     cost_basis: Decimal
     quantity: Decimal
     price: Decimal
-    direction: Literal["buy", "sell"]
+    direction: TDirection
     filled_date: datetime
 
+    def __repr__(self) -> str:
+        return f"Trade({self.symbol}, {self.direction}, {self.filled_date}, {self.quantity})"
 
-@dataclass
+
+@dataclass(order=True)
 class Match:
     order: Order
     trade: Trade
     allocated_quantity: Decimal
+
+    def __repr__(self) -> str:
+        return f"Order: {self.order} | Trade: {self.trade}, Alloc. Quant.: {self.allocated_quantity})"  # noqa: E501
--- order-trade-matching/main.py
+++ order-trade-matching/main.py
@@ -1,24 +1,22 @@
 import pprint
 
-from domain import (
-    TARGET_MATCHES,
-    ORDERS_TO_MATCH,
-    TRADES_TO_MATCH,
-    match_orders_trades,
-)
+from domain import ORDERS_TO_MATCH, TARGET_MATCHES, TRADES_TO_MATCH, match_orders_trades
 
 
 def main():
     matches = match_orders_trades(ORDERS_TO_MATCH, TRADES_TO_MATCH)
     try:
-        assert matches == TARGET_MATCHES, "Orders and trades did not match"
+        assert sorted(matches) == sorted(TARGET_MATCHES), "Not a match!"
     except AssertionError as e:
         print(e)
-        # pp = pprint.PrettyPrinter(width=120, indent=2)
-        # pp.pprint("TARGET MATCHES:")
-        # pp.pprint(TARGET_MATCHES)
-        # pp.pprint("matches:")
-        # pp.pprint(matches)
+        pp = pprint.PrettyPrinter(width=120, indent=2)
+        pp.pprint("Missing solution matches:")
+        for m in (match for match in matches if match not in TARGET_MATCHES):
+            print(m)
+
+        pp.pprint("Missing target matches:")
+        for m in (match for match in TARGET_MATCHES if match not in matches):
+            print(m)
 
 
 if __name__ == "__main__":
